ede3d287d92980f2c5cf12c9d616b6cc95c513a7
almog1her <almog1her@gmail.com>
2022-01-04 13:19:55 +0200

    Change icons

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 let map;
 
-const tourPos = {
-  lat: 32.0707809,
-  lng: 34.8236409
-};
+const ACTIVITY_RADIUS = 20;
+const tourLocations = [
+  {
+    id: 1,
+    name: "מארכז",
+    description: "",
+    activity: "",
+    thumbnail: "",
+    lat: 32.0707809,
+    lng: 34.8236409
+  }
+];
+
+let currLoc;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -19,42 +29,48 @@ function getLocation() {
   }
 }
 
-function haversine_distance(mk1, mk2) {
+function haversineDistance(mk1, mk2) {
   var R = 3958.8; // Radius of the Earth in miles
   var rlat1 = mk1.lat * (Math.PI / 180); // Convert degrees to radians
   var rlat2 = mk2.lat * (Math.PI / 180); // Convert degrees to radians
   var difflat = rlat2 - rlat1; // Radian difference (latitudes)
-  var difflon = (mk2.lng   - mk1.lng) * (Math.PI / 180); // Radian difference (longitudes)
+  var difflon = (mk2.lng - mk1.lng) * (Math.PI / 180); // Radian difference (longitudes)
 
   var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat / 2) * Math.sin(difflat / 2) + Math.cos(rlat1) * Math.cos(rlat2) * Math.sin(difflon / 2) * Math.sin(difflon / 2)));
-  return 1000*d;
+  return 1000 * d;
 }
 
 function showPosition(position) {
-  //let currLoc = { lat: +position.coords.latitude, lng: +position.coords.longitude };
-  let currLoc = {
+  //currLoc = { lat: +position.coords.latitude, lng: +position.coords.longitude };
+  currLoc = {
     lat: 32.0707809,
     lng: 34.8286409
   };
   console.log(currLoc);
-  var mk1 = new google.maps.Marker({ position: currLoc, map: map });
-  var mk2 = new google.maps.Marker({ position: tourPos, map: map });
-  var line = new google.maps.Polyline({path: [currLoc, tourPos], map: map});
-  var latlng = new google.maps.LatLng(currLoc.lat, currLoc.lng);
-  
-  console.log(haversine_distance(currLoc, tourPos));
 
-  map.setCenter(latlng);
-  console.log("center");
+  map.setCenter(currLoc);
 
-  const cityCircle = new google.maps.Circle({
-    strokeColor: "#FF0000",
-    strokeOpacity: 0.2,
-    strokeWeight: 2,
-    fillColor: "#FF0000",
-    fillOpacity: 0.1,
-    map,
-    center: tourPos,
-    radius: 20,
+  const currLocMarker = new google.maps.Marker({
+    position: currLoc, map: map,
+    icon: {
+      url: './misc/my-location-icon.jpg',
+      scaledSize: new google.maps.Size(50, 50)
+    }
   });
+
+  tourLocations.forEach(location => {
+    const mk = new google.maps.Marker({ position: location, map: map });
+    const cityCircle = new google.maps.Circle({
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.2,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.1,
+      map,
+      center: location,
+      radius: ACTIVITY_RADIUS,
+    });
+  });
+
+  console.log(haversineDistance(currLoc, tourPos));
 }
