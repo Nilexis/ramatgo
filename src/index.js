16b3afeddc0ed36adaadb5735797a82674a7e935
Ofir <ofir.duchovne@gmail.com>
2022-01-04 11:23:08 +0200

    set map to user's current location

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    zoom: 18,
   });
+  getLocation();
+}
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } 
+}
+
+function showPosition(position) {
+  var lat = position.coords.latitude;
+  var lng = position.coords.longitude;
+  console.log(lat ,lng);
+  var latlng = new google.maps.LatLng(lat, lng);
+  var marker = new google.maps.Marker({
+    position: latlng,
+    map: map,
+    title: "You are here!",
+  });
+  map.setCenter(latlng);
 }
