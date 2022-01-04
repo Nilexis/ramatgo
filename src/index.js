c81dafd36a11730ad7abb4906908ac8f302bf52b
almog1her <almog1her@gmail.com>
2022-01-04 15:12:21 +0200

    Change location after timeout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let map;
 const ACTIVITY_RADIUS = 20;
 let currLoc;
+let currLocMarker;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -8,22 +9,43 @@ function initMap() {
     zoom: 18,
   });
   getLocation();
+  setTimeout(() => {
+    updatePosition({ lat: +tourLocations[2].lat, lng: +tourLocations[2].lng });
+  }, 20000);
 }
 
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-    navigator.geolocation.watchPosition(updatePosition);
+    // navigator.geolocation.watchPosition(updateGeoPosition);
   }
 }
 
-function updatePosition(geoPos) {
-  position = { lat: +geoPos.coords.latitude, lng: +geoPos.coords.longitude };
+function updateGeoPosition(geoPos) {
+  updatePosition({ lat: +geoPos.coords.latitude, lng: +geoPos.coords.longitude });
+}
+
+function updatePosition(position) {
+  console.log(currLoc);
   if (!currLoc || currLoc.lat !== position.lat || currLoc.lng !== position.lng) {
     currLoc = {
       lat: position.lat,
       lng: position.lng
     };
+
+    if (currLocMarker) {
+      currLocMarker.setMap(null);
+    }
+
+    currLocMarker = new google.maps.Marker({
+      position: currLoc, map: map,
+      icon: {
+        url: './misc/my-location-icon.jpg',
+        scaledSize: new google.maps.Size(50, 50)
+      }
+    });
+
+    currLocMarker.setMap(map);
 
     checkProximity();
   }
@@ -50,16 +72,8 @@ function haversineDistance(mk1, mk2) {
 
 function showPosition(position) {
   // currLoc = { lat: +position.coords.latitude, lng: +position.coords.longitude };
-  updatePosition(position);
+  updateGeoPosition(position);
   map.setCenter(currLoc);
-
-  const currLocMarker = new google.maps.Marker({
-    position: currLoc, map: map,
-    icon: {
-      url: './misc/my-location-icon.jpg',
-      scaledSize: new google.maps.Size(50, 50)
-    }
-  });
 
   tourLocations.forEach(location => {
     const mk = new google.maps.Marker({ position: location, map: map });
